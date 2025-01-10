a6e8faf43b854c1f8bc323dcbba33548ad6065f4
MintBloom <a.stegmann09@gmail.com>
2025-01-10 14:44:30 +0000

    Added rock-paper-scissors file to repository.

---
+++ rock-paper-scissors.py
@@ -0,0 +1,20 @@
+
+import random
+
+#this function is where the user can terminate the program or play one of either rock, paper, or scissors
+def mainmenu():
+    '''the user will be given an option to pick either rock, paper or scissors'''
+    pass
+
+def rock():
+    pass
+
+def paper():
+    pass
+
+def scissors():
+    pass
+
+def comp_choice():
+    choice = random.randint(1,3)
+    return choice 
