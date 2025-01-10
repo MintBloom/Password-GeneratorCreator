bb385707db1f31ba51f947b34548289fd975593b
MintBloom <a.stegmann09@gmail.com>
2025-01-10 19:05:28 +0000

    Updated rock-paper-scissors project

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,20 +1,26 @@
 
 import random
 
-#this function is where the user can terminate the program or play one of either rock, paper, or scissors
-def mainmenu():
-    '''the user will be given an option to pick either rock, paper or scissors'''
-    pass
-
-def rock():
-    pass
-
-def paper():
-    pass
-
-def scissors():
-    pass
+def deciding_outcome():
+    a = [user_choice(),comp_choice()]
+    if a in winning_combinations:
+        print("Congratulations, you have defeated the computer!")
+    elif a in losing_combinations:
+        print("Oop, you have been trounced by the computer.")
+    elif a in draw_combinations:
+        print("Ooo, a tie; try again.")
 
 def comp_choice():
-    choice = random.randint(1,3)
-    return choice 
+    c_choice = random.randint(1,3)
+    return c_choice 
+
+def user_choice():
+    print ("Pick either rock(1), paper(2) or scissors(3)")
+    u_choice = int(input())
+    return u_choice
+
+winning_combinations = ([1,3],[2,1],[3,2])
+losing_combinations = ([1,2],[2,3],[3,1])
+draw_combinations = ([1,1],[2,2],[3,3])
+
+deciding_outcome()
